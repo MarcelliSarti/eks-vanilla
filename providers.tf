248577d12b33ff2bd9c9e16e3499169644a18852
Marcelli Sarti <marcellisarti@hotmail.com>
2026-08-26 16:27:51 -0300

    reconfig helm

--- providers.tf
+++ providers.tf
@@ -1,7 +1,16 @@
-provider "helm" {
-  kubernetes = {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.default.token
+# provider "helm" {
+#   kubernetes = {
+#     host                   = aws_eks_cluster.main.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.default.token
+#   }
+# }
+
+terraform {
+  required_providers {
+    helm = {
+      source                = "hashicorp/helm"
+      configuration_aliases = [helm]
+    }
   }
 }
